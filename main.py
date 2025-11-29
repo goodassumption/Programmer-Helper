1018e4700556706c3c40cd16bd81a23584839936
GoodAssumption <alex.golovin.2010@gmail.com>
2025-11-29 03:20:09 +0300

    feat: Implement core functionality
    This commit introduces the foundational features required for the application to operate. It includes key components and initial setup.
    
    Этот коммит добавляет основные функции, необходимые для работы приложения. Он включает в себя ключевые компоненты и начальную настройку.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import requests
-import API
+import os
 
-api_key = API.api_key
+from src.request import make_request
+from src.choose_module import choose_module
+
+def main():
+    print('Это - помощник программиста для автоматизации различных рутинных задач при помощи нейросетей')
+    choose_module()
+
+if __name__ == '__main__':
+    main()
---
+++ src/request.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from time import sleep
+
+from .API import api_key
+from .utilits import make_log, http_status_descriptions
+
+def make_request(data: dict):
+    url = 'https://chat.fifty.su/api/v1/chat'
+    headers = {
+        'x-api-key': api_key
+    }
+    make_log('Начинаю запрос')
+    try:
+        responce = requests.post(url=url, headers=headers, data=data)
+        status_code = responce.status_code
+        make_log(f'Выполнен запрос по ссылке {url}. Код: {status_code}')
+        make_log(f'{http_status_descriptions.get(status_code, 'Описание не найдено')}')
+
+    except Exception as e:
+        print(f'Произошла непредвиденная ошибка при запросе.')
+        print(f'Текст ошибки: {e}')
+        make_log(f'Произошла непредвиденная ошибка при запросе')
+        make_log(f'Текст ошибки: {e}')
+
+    answer = responce.json()
+    if len(answer) == 1: print('Возникла непредвиденная ошибка!')
+    else: print(answer)
